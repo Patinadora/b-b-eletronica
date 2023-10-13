73e6a38da10fb80f8640e3c1158aff36bac36a95
Patinadora <146873428+Patinadora@users.noreply.github.com>
2023-10-13 09:53:19 -0300

    Add files via upload

---
+++ man.js
@@ -0,0 +1,51 @@
+objectDetector= "";
+ img = "";
+  objects = [];
+   status = "";
+   musica="";
+    function preload(){
+musica=loadSound("oi.mp3");
+     }
+     function setup() { 
+        canvas = createCanvas(640, 420);
+         canvas.center(); 
+         video=createCapture(VIDEO);
+         video.size(640,420);
+video.hide();
+         objectDetector = ml5.objectDetector('cocossd', modelLoaded); 
+         document.getElementById("status").innerHTML = "Status: Detectando Objetos";
+         }
+         function modelLoaded() {
+             console.log("Modelo Carregado!") ;
+             status = "true"; 
+             objectDetector.detect(video, gotResult);
+             }
+             function gotResult(error, results) { 
+                if (error) {
+                     console.log(error);
+                     } console.log(results);
+                      objects = results; 
+            }
+     function draw()
+      { if (status != undefined) { 
+        image(video, 0, 0, 640, 420);
+         for (var i = 0; i < objects.length; i++ ) { 
+                document.getElementById("status").innerHTML = "Status: Objetos Detectados"; 
+
+                fill("#FF0000");
+                 percent = floor(objects[i].confidence * 100);
+                  text(objects[i].label + " " + percent + "%", objects[i].x + 5, objects[i].y + 15);
+                   noFill(); 
+                   stroke(255, 0, 0);
+                    rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+                    if(objects[i].label=="person"){
+                     musica.stop();
+                     document.getElementById("status").innerHTML = "bebe emcontrado"; 
+                    }
+                    else{
+                     musica.play();
+                     document.getElementById("status").innerHTML = "bebe não emcontrado";
+                    }
+     }
+     }
+     }
